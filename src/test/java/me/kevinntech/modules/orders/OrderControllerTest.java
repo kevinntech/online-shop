1be524f2c60a937a84a9f4d7980906b086fda32d
kevinntech <kevinntech@nate.com>
2021-05-01 23:16:11 +0900

    주문서 조회 테스트 코드 작성

---
+++ src/test/java/me/kevinntech/modules/orders/OrderControllerTest.java
@@ -0,0 +1,58 @@
+package me.kevinntech.modules.orders;
+
+import me.kevinntech.infra.MockMvcTest;
+import me.kevinntech.modules.products.Product;
+import me.kevinntech.modules.products.ProductFactory;
+import me.kevinntech.modules.products.dto.ProductToOrderForm;
+import me.kevinntech.modules.users.WithUser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@MockMvcTest
+class OrderControllerTest {
+
+    @Autowired MockMvc mockMvc;
+    @Autowired ProductFactory productFactory;
+
+    @Test
+    @WithUser("kevin")
+    @DisplayName("주문서 폼 조회 - 완료")
+    void saveNewOrderForm_success() throws Exception {
+        Product product = productFactory.createProduct();
+
+        ProductToOrderForm responseDto = new ProductToOrderForm(product, 10);
+        List<ProductToOrderForm> orderList = new ArrayList<>();
+        orderList.add(responseDto);
+
+        mockMvc.perform(get("/orders/new")
+                    .flashAttr("orderList", orderList))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orders/new"))
+                .andExpect(model().attributeExists("orderList"))
+                .andExpect(model().attributeExists("totalPaymentAmount"));
+    }
+
+    @Test
+    @WithUser("kevin")
+    @DisplayName("주문서 폼 조회 - 실패")
+    void saveNewOrderForm_fail() throws Exception {
+        Product product = productFactory.createProduct();
+
+        ProductToOrderForm responseDto = new ProductToOrderForm(product, 0);
+        List<ProductToOrderForm> orderList = new ArrayList<>();
+        orderList.add(responseDto);
+
+        mockMvc.perform(get("/orders/new")
+                    .flashAttr("orderList", orderList))
+                .andExpect(status().is3xxRedirection());
+    }
+
+}
